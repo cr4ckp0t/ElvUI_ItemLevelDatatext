1a8830bfd0ef6fcef277fa7f6015cbd934757cf5
Adam Koch <me@adamko.ch>
2019-05-15 20:49:34 -0400

    Initial release for BFA.

---
+++ Locales/ptBR.lua
@@ -0,0 +1,8 @@
+-------------------------------------------------------------------------------
+-- ElvUI Item Level Datatext By Lockslap (US, Bleeding Hollow)
+--------------------------------------------------------------------------------
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("ElvUI_ItemLevelDatatext", "ptBR", false)
+if not L then return end
+
+--@localization(locale="ptBR", format="lua_additive_table", same-key-is-true=true, escape-non-ascii=true, handle-unlocalized="english")@
