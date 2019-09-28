72376b029dd66b1d935e23f844244f495df51742
Adam Koch <me@adamko.ch>
2019-09-28 11:43:06 -0400

    Updated localizations.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,33 +1,44 @@
 -------------------------------------------------------------------------------
--- ElvUI_ItemLevelDatatext By Crackpotx (US, Lightbringer)
+-- ElvUI ItemLevelDatatext By Crackpotx (US, Lightbringer)
 -------------------------------------------------------------------------------
-local debug = false
---@debug@
-debug = true
---@end-debug@
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
-local L = AceLocale:NewLocale("ElvUI_ItemLevelDatatext", "enUS", true, debug)
+local L = AceLocale:NewLocale("ElvUI_ItemLevelDatatext", "enUS", true, true)
 if not L then return end
 
+L["Alt + Click to Delete"] = true
+L["Are you sure you want to delete %s?"] = true
 L["Back"] = true
+L["Change Equipment Set"] = true
 L["Chest"] = true
+L["Choose Equipment Set"] = true
+L["Ctrl + Click to Save"] = true
+L["Deleted equipment set |cff%s%s|r!"] = true
+L["Equipment Set"] = true
 L["Equipped"] = true
 L["Feet"] = true
 L["Hands"] = true
 L["Head"] = true
 L["Item Level Datatext"] = true
 L["Item Level"] = true
+L["Left Click"] = true
 L["Legs"] = true
 L["Main Hand"] = true
 L["Neck"] = true
+L["No Equipment Sets"] = true
+L["None Equipped"] = true
 L["Number of decimal places to round to for the average item level."] = true
 L["Off Hand"] = true
+L["Open Character Pane"] = true
 L["Plugins by |cff9382c9Crackpotx|r"] = true
 L["Precision"] = true
 L["Ranged"] = true
+L["Rename %s to what?"] = true
+L["Renamed |cff%s%s|r to |cff%s%s|r!"] = true
+L["Right Click"] = true
 L["Ring 1"] = true
 L["Ring 2"] = true
 L["Select which item level you want to display in the datatext, total or equipped."] = true
+L["Shift + Click to Rename"] = true
 L["Shoulders"] = true
 L["Show Item Name"] = true
 L["Show item name in the tooltip."] = true
