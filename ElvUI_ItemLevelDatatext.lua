ee5fa2d8abe731f2fc9899f647e700326257ba17
Adam Koch <me@adamko.ch>
2021-07-01 10:28:21 -0400

    Update TOC for 9.1.

--- ElvUI_ItemLevelDatatext.lua
+++ ElvUI_ItemLevelDatatext.lua
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- ElvUI Item Level Datatext By Crackpotx
+-- ElvUI Item Level Datatext By Crackpot
 -------------------------------------------------------------------------------
 local E, _, _, P, _, _ = unpack(ElvUI)
 local DT = E:GetModule("DataTexts")
@@ -51,7 +51,7 @@ local slots = {
 	[14] = L["Trinket 2"],
 	[15] = L["Back"],
 	[16] = L["Main Hand"],
-	[17] = L["Off Hand"],
+	[17] = L["Off Hand"]
 	--[18] = L["Ranged"],
 }
 
@@ -68,10 +68,13 @@ StaticPopupDialogs["ILDT_RENAME"] = {
 	maxLetters = 16,
 	exclusive = 0,
 	preferredIndex = 3,
-	timeout = 0, 
+	timeout = 0,
 	whileDead = true,
 	hideOnEscape = true,
-	OnShow = function(self) _G[self:GetName() .. "EditBox"]:SetFocus(); self.button1:Disable() end,
+	OnShow = function(self)
+		_G[self:GetName() .. "EditBox"]:SetFocus()
+		self.button1:Disable()
+	end,
 	OnHide = function(self)
 		if _G[self:GetName() .. "EditBox"]:IsShown() then
 			_G[self:GetName() .. "EditBox"]:SetFocus()
@@ -81,25 +84,35 @@ StaticPopupDialogs["ILDT_RENAME"] = {
 	OnAccept = function(self, setId)
 		local newName = _G[self:GetName() .. "EditBox"]:GetText()
 		local oldName = C_EquipmentSet_GetEquipmentSetInfo(setId)
-		if not newName or newName == "" or oldName == newName then return end
+		if not newName or newName == "" or oldName == newName then
+			return
+		end
 		C_EquipmentSet_ModifyEquipmentSet(setId, newName)
-		DEFAULT_CHAT_FRAME:AddMessage(chatString:format((L["Renamed |cff%s%s|r to |cff%s%s|r!"]):format(hexColor, oldName, hexColor, newName)))
+		DEFAULT_CHAT_FRAME:AddMessage(
+			chatString:format((L["Renamed |cff%s%s|r to |cff%s%s|r!"]):format(hexColor, oldName, hexColor, newName))
+		)
 	end,
 	EditBoxOnEnterPressed = function(self, setId)
 		local newName = self:GetText()
 		local oldName = C_EquipmentSet_GetEquipmentSetInfo(setId)
-		if not newName or newName == "" or oldName == newName then return end
+		if not newName or newName == "" or oldName == newName then
+			return
+		end
 		C_EquipmentSet_ModifyEquipmentSet(setId, newName)
-		DEFAULT_CHAT_FRAME:AddMessage(chatString:format((L["Renamed |cff%s%s|r to |cff%s%s|r!"]):format(hexColor, oldName, hexColor, newName)))
+		DEFAULT_CHAT_FRAME:AddMessage(
+			chatString:format((L["Renamed |cff%s%s|r to |cff%s%s|r!"]):format(hexColor, oldName, hexColor, newName))
+		)
 		self:GetParent():Hide()
 	end,
-	EditBoxOnEscapePressed = function(self) self:GetParent():Hide() end,
+	EditBoxOnEscapePressed = function(self)
+		self:GetParent():Hide()
+	end,
 	EditBoxOnTextChanged = function(self)
 		local parent = self:GetParent()
 		if _G[parent:GetName() .. "EditBox"]:GetText() == "" then
 			parent.button1:Disable()
 		else
-			parent.button1:Enable();
+			parent.button1:Enable()
 		end
 	end
 }
@@ -116,7 +129,7 @@ StaticPopupDialogs["ILDT_DELETE"] = {
 		local oldName = C_EquipmentSet_GetEquipmentSetInfo(setId)
 		C_EquipmentSet_DeleteEquipmentSet(setId)
 		DEFAULT_CHAT_FRAME:AddMessage(chatString:format((L["Deleted equipment set |cff%s%s|r!"]):format(hexColor, oldName)))
-	end,
+	end
 }
 
 -- rounds a number for printing
@@ -126,7 +139,9 @@ end
 
 local function GetEquippedSet()
 	local num = C_EquipmentSet_GetNumEquipmentSets()
-	if num == 0 then return false end
+	if num == 0 then
+		return false
+	end
 	for i = 1, num do
 		local name, icon, _, isEquipped = C_EquipmentSet_GetEquipmentSetInfo(i)
 		if isEquipped == true then
@@ -162,14 +177,36 @@ end
 local function OnEvent(self, event)
 	lastPanel = self
 	local total, equipped = GetAverageItemLevel()
-	self.text:SetFormattedText(displayString, L["Item Level"], E.db.ilvldt.ilvl == "equip" and DecRound(equipped, E.db.ilvldt.precision) or DecRound(total, E.db.ilvldt.precision))
+	self.text:SetFormattedText(
+		displayString,
+		L["Item Level"],
+		E.db.ilvldt.ilvl == "equip" and DecRound(equipped, E.db.ilvldt.precision) or DecRound(total, E.db.ilvldt.precision)
+	)
 end
 
 local function OnEnter(self)
 	local total, equipped = GetAverageItemLevel()
 	DT:SetupTooltip(self)
-	DT.tooltip:AddDoubleLine(L["Total"], DecRound(total, E.db.ilvldt.precision), 1, 1, 1, rgbColor.r, rgbColor.g, rgbColor.b)
-	DT.tooltip:AddDoubleLine(L["Equipped"], DecRound(equipped, E.db.ilvldt.precision), 1, 1, 1, rgbColor.r, rgbColor.g, rgbColor.b)
+	DT.tooltip:AddDoubleLine(
+		L["Total"],
+		DecRound(total, E.db.ilvldt.precision),
+		1,
+		1,
+		1,
+		rgbColor.r,
+		rgbColor.g,
+		rgbColor.b
+	)
+	DT.tooltip:AddDoubleLine(
+		L["Equipped"],
+		DecRound(equipped, E.db.ilvldt.precision),
+		1,
+		1,
+		1,
+		rgbColor.r,
+		rgbColor.g,
+		rgbColor.b
+	)
 	if C_EquipmentSet_GetNumEquipmentSets() > 0 then
 		DT.tooltip:AddDoubleLine(L["Equipment Set"], GetEquippedSet(), 1, 1, 1, rgbColor.r, rgbColor.g, rgbColor.b)
 	end
@@ -178,7 +215,18 @@ local function OnEnter(self)
 		if slots[i] and GetInventoryItemID("player", i) then
 			local name, _, quality, _, _, _, _, _, _, _, _ = GetItemInfo(GetInventoryItemLink("player", i))
 			local red, green, blue, _ = GetItemQualityColor(quality)
-			DT.tooltip:AddDoubleLine(slots[i], E.db.ilvldt.showItem == true and ("%s (%d)"):format(name, GetDetailedItemLevelInfo(GetInventoryItemLink("player", i))) or GetDetailedItemLevelInfo(GetInventoryItemLink("player", i)), 1, 1, 1, red, green, blue)
+			DT.tooltip:AddDoubleLine(
+				slots[i],
+				E.db.ilvldt.showItem == true and
+					("%s (%d)"):format(name, GetDetailedItemLevelInfo(GetInventoryItemLink("player", i))) or
+					GetDetailedItemLevelInfo(GetInventoryItemLink("player", i)),
+				1,
+				1,
+				1,
+				red,
+				green,
+				blue
+			)
 		end
 	end
 	DT.tooltip:AddLine(" ")
@@ -192,13 +240,13 @@ local function OnClick(self, button)
 		ToggleCharacter("PaperDollFrame")
 	elseif button == "RightButton" then
 		DT.tooltip:Hide()
-		
-		local menuList = {{text = L["Choose Equipment Set"], isTitle = true, notCheckable = true,},}
+
+		local menuList = {{text = L["Choose Equipment Set"], isTitle = true, notCheckable = true}}
 		local numSets = C_EquipmentSet_GetNumEquipmentSets()
 		local color = "ffffff"
-		
+
 		if not numSets or tonumber(numSets) == 0 then
-			menuList[#menuList + 1] = {text = ("|cffff0000%s|r"):format(L["No Equipment Sets"]), notCheckable = true,}
+			menuList[#menuList + 1] = {text = ("|cffff0000%s|r"):format(L["No Equipment Sets"]), notCheckable = true}
 		else
 			for i = 0, numSets do
 				local name, _, _, isEquipped, _, _, _, missing, _ = C_EquipmentSet_GetEquipmentSetInfo(i)
@@ -209,32 +257,57 @@ local function OnClick(self, button)
 						color = isEquipped == true and hexColor or "ffffff"
 					end
 
-					menuList[#menuList + 1] = {text = ("|cff%s%s|r"):format(color, name), func = EquipmentSetClick, arg1 = name, checked = isEquipped == true and true or false,}
+					menuList[#menuList + 1] = {
+						text = ("|cff%s%s|r"):format(color, name),
+						func = EquipmentSetClick,
+						arg1 = name,
+						checked = isEquipped == true and true or false
+					}
 				end
 			end
-			
+
 			-- add a hint
-			menuList[#menuList + 1] = {text = L["Shift + Click to Rename"], isTitle = true, notCheckable = true, notClickable = true}
-			menuList[#menuList + 1] = {text = L["Ctrl + Click to Save"], isTitle = true, notCheckable = true, notClickable = true}
-			menuList[#menuList + 1] = {text = L["Alt + Click to Delete"], isTitle = true, notCheckable = true, notClickable = true}
+			menuList[#menuList + 1] = {
+				text = L["Shift + Click to Rename"],
+				isTitle = true,
+				notCheckable = true,
+				notClickable = true
+			}
+			menuList[#menuList + 1] = {
+				text = L["Ctrl + Click to Save"],
+				isTitle = true,
+				notCheckable = true,
+				notClickable = true
+			}
+			menuList[#menuList + 1] = {
+				text = L["Alt + Click to Delete"],
+				isTitle = true,
+				notCheckable = true,
+				notClickable = true
+			}
 		end
 		EasyMenu(menuList, menuFrame, "cursor", 0, 0, "MENU", 2)
 	end
-	
+
 	OnEvent(self)
 end
 
 local interval = 10
 local function OnUpdate(self, elapsed)
-	if not self.lastUpdate then self.lastUpdate = 0 end
+	if not self.lastUpdate then
+		self.lastUpdate = 0
+	end
 	self.lastUpdate = self.lastUpdate + elapsed
 	if self.lastUpdate > interval then
 		self.lastUpdate = 0
 		local total, equipped = GetAverageItemLevel()
-		self.text:SetFormattedText(displayString, L["Item Level"], E.db.ilvldt.ilvl == "equip" and DecRound(equipped, E.db.ilvldt.precision) or DecRound(total, E.db.ilvldt.precision))
+		self.text:SetFormattedText(
+			displayString,
+			L["Item Level"],
+			E.db.ilvldt.ilvl == "equip" and DecRound(equipped, E.db.ilvldt.precision) or DecRound(total, E.db.ilvldt.precision)
+		)
 	end
 end
-
 
 local function ValueColorUpdate(hex, r, g, b)
 	displayString = join("", "|cffffffff%s:|r", " ", hex, "%s|r")
@@ -242,7 +315,7 @@ local function ValueColorUpdate(hex, r, g, b)
 	rgbColor = {
 		r = r,
 		g = g,
-		b = b,
+		b = b
 	}
 	if lastPanel ~= nil then
 		OnEvent(lastPanel, "ELVUI_COLOR_UPDATE")
@@ -253,7 +326,7 @@ E["valueColorUpdateFuncs"][ValueColorUpdate] = true
 P["ilvldt"] = {
 	["ilvl"] = "equip",
 	["precision"] = 2,
-	["showItem"] = true,
+	["showItem"] = true
 }
 
 local function InjectOptions()
@@ -266,23 +339,32 @@ local function InjectOptions()
 				thanks = {
 					type = "description",
 					order = 1,
-					name = L["Thanks for using and supporting my work!  -- |cff0070deCrackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."],
-				},
-			},
+					name = L[
+						"Thanks for using and supporting my work!  -- |cff0070deCrackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."
+					]
+				}
+			}
 		}
 	elseif not E.Options.args.Crackpotx.args.thanks then
 		E.Options.args.Crackpotx.args.thanks = {
 			type = "description",
 			order = 1,
-			name = L["Thanks for using and supporting my work!  -- |cff0070deCrackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."],
+			name = L[
+				"Thanks for using and supporting my work!  -- |cff0070deCrackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."
+			]
 		}
 	end
 
 	E.Options.args.Crackpotx.args.ilvldt = {
 		type = "group",
 		name = L["Item Level Datatext"],
-		get = function(info) return E.db.ilvldt[info[#info]] end,
-		set = function(info, value) E.db.ilvldt[info[#info]] = value; DT:LoadDataTexts() end,
+		get = function(info)
+			return E.db.ilvldt[info[#info]]
+		end,
+		set = function(info, value)
+			E.db.ilvldt[info[#info]] = value
+			DT:LoadDataTexts()
+		end,
 		args = {
 			ilvl = {
 				type = "select",
@@ -291,26 +373,38 @@ local function InjectOptions()
 				desc = L["Select which item level you want to display in the datatext, total or equipped."],
 				values = {
 					["equip"] = L["Equipped"],
-					["total"] = L["Total"],
-				},
+					["total"] = L["Total"]
+				}
 			},
 			precision = {
 				type = "range",
 				order = 5,
 				name = L["Precision"],
 				desc = L["Number of decimal places to round to for the average item level."],
-				min = 0, max = 5, step = 1,
+				min = 0,
+				max = 5,
+				step = 1
 			},
 			showItem = {
 				type = "toggle",
 				order = 6,
 				name = L["Show Item Name"],
-				desc = L["Show item name in the tooltip."],
-			},
-		},
+				desc = L["Show item name in the tooltip."]
+			}
+		}
 	}
 end
 
 EP:RegisterPlugin(..., InjectOptions)
-DT:RegisterDatatext(L["Item Level (Improved)"], nil, {"PLAYER_ENTERING_WORLD"}, OnEvent, OnUpdate, OnClick, OnEnter, nil, L["Item Level (Improved)"])
+DT:RegisterDatatext(
+	L["Item Level (Improved)"],
+	nil,
+	{"PLAYER_ENTERING_WORLD"},
+	OnEvent,
+	OnUpdate,
+	OnClick,
+	OnEnter,
+	nil,
+	L["Item Level (Improved)"]
+)
 --DT:RegisterDatatext(L["Item Level (Improved)"], {"PLAYER_ENTERING_WORLD"}, OnEvent, OnUpdate, OnClick, OnEnter)
